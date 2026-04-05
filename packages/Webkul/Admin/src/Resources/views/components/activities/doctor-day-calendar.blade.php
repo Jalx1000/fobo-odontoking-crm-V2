ce166e516348254c6aa6b4d58d0fbe8294c06f2e
jalx1000 <jalejandromogro1@gmail.com>
2026-04-06 00:29:00 +0200

    fix: prevent negative count in doctor day calendar display
    Ensure the count displayed for remaining activities does not show a negative number when there are 2 or fewer items. This improves user experience by avoiding confusing negative values in the UI.

--- packages/Webkul/Admin/src/Resources/views/components/activities/doctor-day-calendar.blade.php
+++ packages/Webkul/Admin/src/Resources/views/components/activities/doctor-day-calendar.blade.php
@@ -11,7 +11,7 @@
                 </span> --}}
             </div>
             <div class="ms-actions">
-                <span class="ms-count">@{{ model.length - 2 }}</span>
+                <span class="ms-count">@{{ model.length > 2 ? model.length - 2 : 0 }}</span>
                 <i :class="open ? 'icon-up-arrow' : 'icon-down-arrow'" class="text-xl"></i>
             </div>
         </div>
